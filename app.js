d5f329eca8b2b3e605f4587b30a5cb6c0f5387c2
krista triviso <krista.triviso91@gmail.com>
2018-04-02 17:18:58 -0400

    collision detection

--- app.js
+++ app.js
@@ -1,58 +1,68 @@
-$(document).ready(function(){
-    const container = $('.container')
-    const spaceship = $('img')
-    const startButton = $('button')
-    const body = $('body')
+$(document).ready(function() {
 
-    startButton.on('click', function(){
-        $(spaceship).attr({'src': 'ufo_1.svg', 'id': 'spaceship'})
-    });
+  const container = $('.container')
+  const containerLocation = container[0].getBoundingClientRect()
+  const spaceship = $('img')
+  const startButton = $('button')
+  const body = $('body')
+  const asteroidCollection = document.getElementsByClassName('asteroid')
 
-    // how many asteroids you want
-    for(let i = 0; i < 8; i++){
-        makeasteroid();
+  $(spaceship).attr({
+    'src': 'ufo_1.svg',
+    'id': 'spaceship'
+  })
+
+  // how many asteroids are made
+  for (let i = 0; i < 6; i++) {
+    makeAsteroid()
+  }
+
+  function choose() {
+    for (let j = 0; j < asteroidCollection.length; j++) {
+      setInterval(collision(spaceship[0].getBoundingClientRect(), asteroidCollection[j].getBoundingClientRect()), 1000)
     }
+  }
+  // setInterval(choose, 1000)
 
-    function makeasteroid(){
-        let id = Math.floor(Math.random()*999999) + 100000
-        let asteroid = $('<img></img>').attr({'id': id, 'src': 'as1.svg'})
-        // get random coordinates every time you make an asteroid
-        let top = Math.random() * 480
-        // if the id is even, start from left, if odd start from right
-        if(asteroid.attr('id') % 2 === 0){
-            asteroid.attr('class', 'left-asteroid asteroid')
-        } else if (asteroid.attr('id') % 3 === 0){
-            asteroid.attr('class', 'slow-asteroid asteroid')
-        } else if (asteroid.attr('id') % 5 === 0){
-            asteroid.attr('class', 'fast-asteroid asteroid')
-        } else{
-            asteroid.attr('class', 'right-asteroid asteroid')
-        }
-        // window.innerHeight
-        container.append(asteroid)
-        $(asteroid).css('top', top)
+  function collision(rect1, rect2) {
+    if (rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x && rect1.y < rect2.y + rect2.height && rect1.height + rect1.y > rect2.y) {
+      console.log('collision')
     }
+  }
 
-    // grab arrow key events
-    // Up: 38 - Down: 40 - Right: 39 - Left: 37
-    $(document).keyup(function(e) {
-        let currentX = spaceship.css('bottom')
-        let currentY = spaceship.css('right')
-        let configX = parseInt(currentX.replace('px', ''))
-        let configY = parseInt(currentY.replace('px', ''))
-        if (e.which === 38) {
-            configX += 15
-            spaceship.css('bottom', configX)
-        } else if (e.which === 40) {
-            configX -= 15
-            spaceship.css('bottom', configX)
-        } else if (e.which === 37) {
-            configY += 30
-            spaceship.css('right', configY)
-        } else if (e.which === 39) {
-            configY -= 30
-            spaceship.css('right', configY)
-        }
-    });
+  function makeAsteroid() {
+    let asteroid = $('<img></img>').attr({
+      'class': 'asteroid',
+      'src': 'as1.svg'
+    })
+    let top = Math.random() * containerLocation.height
+    let left = Math.random() * containerLocation.width
+    container.append(asteroid)
+    $(asteroid).css({
+      'top': top,
+      'left': left
+    })
+  }
+
+  // grab arrow key events
+  $(document).keyup(function(e) {
+    let currentX = spaceship.css('bottom')
+    let currentY = spaceship.css('right')
+    let configX = parseInt(currentX.replace('px', ''))
+    let configY = parseInt(currentY.replace('px', ''))
+    if (e.which === 38) {
+      configX += 15
+      spaceship.css('bottom', configX)
+    } else if (e.which === 40) {
+      configX -= 15
+      spaceship.css('bottom', configX)
+    } else if (e.which === 37) {
+      configY += 30
+      spaceship.css('right', configY)
+    } else if (e.which === 39) {
+      configY -= 30
+      spaceship.css('right', configY)
+    }
+  });
 
 })
